e65f41b4711737ba8c78bd93817ff5a86e32e969
Ludvig <Luddesnek@protonmail.com>
2021-11-06 00:11:24 +0100

    Add files via upload

---
+++ dotfiles/fish/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+
+set fish_greeting
+alias ytv="yt-dlp -f 248+251"
+alias yta="yt-dlp -f 251"
+alias mnv="mpv --no-video"
+alias fs="import -window root screenshot.png"
+alias cl="celluloid"
+
+cat ~/.cache/wal/sequences
+
+end
+
+
+
+
+
+
+
+
