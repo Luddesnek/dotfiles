78fbb0fbacd0a954fa94643f4e77990812e4cde4
Lu <Luddesnek@protonmail.com>
2022-04-14 18:21:52 +0200

    Add files via upload

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Ubuntu Mono:pixelsize=22:antialias=true:autohint=true";
+static char *font = "Ubuntu Mono:pixelsize=23:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -156,8 +156,8 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_O,           zoom,           {.f = +1} },
+	{ TERMMOD,              XK_L,           zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
