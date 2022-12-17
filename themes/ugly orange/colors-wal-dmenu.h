dcdef88275bde55fee90549f21524ebfa0257325
Lu <Luddesnek@protonmail.com>
2022-12-17 22:30:47 +0100

    Delete themes/ugly orange directory

--- themes/ugly orange/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#b3b9d9", "#090a0d" },
-	[SchemeSel] = { "#b3b9d9", "#624774" },
-	[SchemeOut] = { "#b3b9d9", "#9E628B" },
-};
