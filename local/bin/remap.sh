178dc309fc9046baf40defe55612ca6903642cd6
Lu <Luddesnek@proton.me>
2022-12-18 09:11:49 +0100

    Add files via upload

--- local/bin/remap.sh
+++ local/bin/remap.sh
@@ -4,7 +4,7 @@ xset r rate 250 33
 xinput set-prop "pointer:Logitech K400" "Coordinate Transformation Matrix"\
 	2.6 0 0 0 2.6 0 0 0 1
 
-setxkbmap -option "caps:super"
+setxkbmap -option "caps:escape"
 
 xmodmap -e "keycode 62 = slash"
 
